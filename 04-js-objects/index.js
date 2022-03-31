43ef0f573228a3c68043989c859d12505986b357
Elna JP <eriosdd@gmail.com>
2022-03-31 18:44:13 -0400

    Updated

--- 04-js-objects/index.js
+++ 04-js-objects/index.js
@@ -1,2 +1,18 @@
+//#1 question//
+let add =(num1, num2) => num1 + num2;
+// correct #1 answer [const add = (num1 , num2) => num1 + num2]//
+//#2 question//
+let sub =(num1 , num2) => num1 - num2;
+//correct answer to #2// [function sub(num1 , num2){ return Math.abs(num1 -num2);} ] (Math.abs used for absolute positive value)//
 
-  
+//#3 answer//
+const compute = (callback) => callback(6 , 3)
+
+//#3a answer//
+console.log(compute(sub))
+// #4 answer//
+compute(function sub(num1 , num2){ 
+    return Math.abs(num1 - num2);
+})
+//#5 answer//
+setInterval(function(){console.log("Hi")}, 3000)
--- 04-js-objects/studentNotes.js
+++ 04-js-objects/studentNotes.js
@@ -1,6 +1,54 @@
 // this is where you can take notes without risking merge conflicts when you pull down new code from Github (the remote is called origin => i.e., where your online repo for this directory is located)
+//const arr = [ 1, 2 , 3, 4]
+//const me ={name: "Elna", job: "teacher", faveFood: "tofu"}
 
+//me["update-name"] = "Soleil"
+//let friend = 'josh'
+//me[friend] = josh
 // also feel free to use this as a sandbox!
+
+//Pokemon ASSIGNMENT: Define a pokemon variable and assign it to an object containing name, img, and likes keys. The value of likes should be 0 while name and img can be whatever pokemon character data you like. 'img' should be an address as a string. (Doesnt have to be a working url)
+
+// const pokemon = {
+  //  name: "Pikachu", 
+ //   img: "https://imageofPikachu.com", 
+  //  likes: 0
+// }
+// 
+// const pokemon = {
+  //  name : "Growlithe" ,
+ //   likes: 0,
+  //  img : "https://bulbapedia.bulbagarden.net/wiki/Growlithe_(Pok%C3%A9mon)",
+//}
+//
+
+//What would happen if we set a name key = to a value and then try to initialize another key with the same name?
+//Object.key -exact key name
+//object[key] // if using a variable
+// object["new-key"] - if key contains non-standard chars
+// *(same as spread)* object.assign(target, existingObj, newProps)
+
+// Define a function increaseValue() that should take 2 parameters: 'object' and 'key' by 1 so that every time the function is invoked, the number of likes will increment.
+//function increaseValue(obj , key) {
+ //   return obj[key]++
+                 //+= 1 (works too)
+//}
+
+//increaseValue(pokemon, "likes")
+
+//const pokemon = {
+   // name: 'pikachu',
+   // age:7,
+   // likes: 0,
+//}
+
+
+
+// **** assignmment due thursday: -- create a new complex object that fills in data to represent you, 
+//and practice accessing various properties manually. 
+//Then write functions to try to access those same values:*****//
+
+
 
 // Running the file ```node notes.js``` (without the backticks) will allow you to explore/play around with code. Make sure you have cd'ed into the inner folder before you do this.
 
